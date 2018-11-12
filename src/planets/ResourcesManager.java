ea265df1d0279a8b0ff56294395d3ec5825a5ec8
xDysta <xav4098@gmail.com>
2018-11-12 17:57:54 +0100

    Implement ResourcesManager

--- src/planets/ResourcesManager.java
+++ src/planets/ResourcesManager.java
@@ -1,9 +1,41 @@
 package planets;
 
+import alien.Game;
+import javafx.scene.image.Image;
+
 public class ResourcesManager {
 	
-	// public static attributes to access resources
+	public static Sprite ship;
 	
-	// public methods to load attributes depending on context
+	public static Sprite planet;
+	
+	public static Image background;	
+	
+	/* =================== */
+	
+	public static String getRessourcePathByName(String name) {
+		return Game.class.getResource('/' + name).toString();
+	}
+	
+	/* ====== GAME ASSETS ====== */
+
+	public static void initGameAssets(double width, double height) {
+		ResourcesManager.loadBackground("images/background.jpg", width, height, false, false);
+		ResourcesManager.loadPlanet("images/planet.svg", 200, 200, width, height);
+		ResourcesManager.loadShip("images/ship.svg", 200, 200, width, height);
+	}
+	
+	private static void loadBackground(String path, double width, double height, boolean preserveRatio, boolean smooth) {
+		ResourcesManager.background = new Image(getRessourcePathByName(path), width, height, preserveRatio, smooth);
+	}
+	
+	private static void loadShip(String path, int width, int height, double maxX, double maxY) {
+		ResourcesManager.ship = new Sprite(getRessourcePathByName(path), width, height, maxX, maxY);
+	}
+	
+	private static void loadPlanet(String path, int width, int height, double maxX, double maxY) {
+		ResourcesManager.planet = new Sprite(getRessourcePathByName(path), width, height, maxX, maxY);
+	}
+	
 	
 }
