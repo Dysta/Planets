fca6aede6aaeee925063c596f2f02d43924bd6ec
xDysta <xav4098@gmail.com>
2018-11-12 18:20:40 +0100

    implement planet.java

--- src/planets/Entities/Planet.java
+++ src/planets/Entities/Planet.java
@@ -3,6 +3,11 @@ package planets.Entities;
 import planets.Sprite;
 
 public class Planet extends Sprite {
+	
+	private boolean isConquered;
+	private int posX, posY, size;
+	// private type des vaisseaux
+	private int timeToProduct;
 
 	public Planet(Sprite s) {
 		super(s);
