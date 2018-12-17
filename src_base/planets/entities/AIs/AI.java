6e1d7f447b7e87a2ec0fa608d4c761f256d1cf2b
Unknown <adrien.boitelle@gmail.com>
2018-12-17 22:52:46 +0100

    AIs javadoc

--- src_base/planets/entities/AIs/AI.java
+++ src_base/planets/entities/AIs/AI.java
@@ -8,13 +8,42 @@ import planets.entities.Player;
 import planets.entities.planet.Planet;
 import planets.utils.GameUtils;
 
+/**
+ * This is a Player controlled by code
+ * 
+ * @author Adri
+ */
 abstract public class AI extends Player {
 
+    /**
+     * Risk and reward as well as overall courage.
+     */
     private int aggressivity;
+    
+    /**
+     * The contrary of aggressivity.
+     */
     private int cautiousness;
+    
+    /**
+     * The ability to assess a situation.
+     */
     private int wisdom;
+    
+    /**
+     * The amount of security the AI needs before validating its decision.
+     */
     private int doubt;
 
+    /**
+     * Creates an AI according to minimalistic characterstics and its color.
+     * 
+     * @param color This player's color
+     * @param aggressivity The aggressivity characteristic
+     * @param cautiousness The cautiousness characteristic
+     * @param wisdom The wisdom characteristic
+     * @param doubt  The doubt characteristic
+     */
     public AI(Color color, int aggressivity, int cautiousness, int wisdom, int doubt) {
         super(color, true);
         this.aggressivity = aggressivity;
@@ -23,6 +52,9 @@ abstract public class AI extends Player {
         this.doubt = doubt;
     }
 
+    /**
+     * The actions performed at each AI_THINK tick
+     */
     @Override
     public void handle() {
         for (Planet p : Galaxy.getPlanets()) {
@@ -43,18 +75,46 @@ abstract public class AI extends Player {
         }
     }
 
+    /**
+     * Compares two arbitrary forces according to its characteristics.
+     * 
+     * @param me The AIs strength
+     * @param enemy The opposing strength
+     * @return The difference in strength according to the AI
+     */
     private double forceCompare(double me, double enemy) {
         return (me + (this.aggressivity / this.wisdom)) / (enemy + this.cautiousness);
     }
 
+    /**
+     * Takes a decision according to its characteristics.
+     * 
+     * @param odds The arbitrary odds for the decision
+     * @return Whether this decision is taken or not
+     */
     private boolean decide(double odds) {
         return (odds - (this.doubt / 100)) > 1;
     }
 
+    /**
+     * Makes the AI compare two planets' strength.
+     * 
+     * @param p Its own planet
+     * @param t The potential target
+     * @return The difference in strength according to the AI
+     */
     private double percentInvade(Planet p, Planet t) {
         return forceCompare(p.getPower(), t.getPower());
     }
 
+
+    /**
+     * Makes the AI compare a group invasion against a single planet
+     * 
+     * @param ps The originating planets collection
+     * @param t The target
+     * @return The difference in strength according to the AI
+     */
     private double percentGroupInvade(ArrayList<Planet> ps, Planet t) {
         int forces = 0;
 
@@ -65,14 +125,31 @@ abstract public class AI extends Player {
         return forceCompare(forces, t.getPower());
     }
 
+    /**
+     * Decides if the AI should start an invasion of a planet from one of his own
+     * 
+     * @param p Its own planet
+     * @param t The target
+     * @return Whether the decision is taken
+     */
     private boolean shouldInvade(Planet p, Planet t) {
         return decide(percentInvade(p, t));
     }
     
+    /**
+     * Returns true, as opposed to a simple Player object.
+     * 
+     * @return true
+     */
     @Override
     public boolean isAI() {
         return true;
     }
     
+    /**
+     * Forces the extending methods to make their classpath available.
+     * 
+     * @return its own classpath
+     */
     abstract public String AIclass();
 }
--- src_base/planets/entities/AIs/BaseAI.java
+++ src_base/planets/entities/AIs/BaseAI.java
@@ -8,15 +8,26 @@ package planets.entities.AIs;
 import javafx.scene.paint.Color;
 
 /**
- *
+ *  This is a very basic AI with basic behavior and a very neutral position towards the game.
+ * 
  * @author Adri
  */
 public class BaseAI extends AI {
 
+    /**
+     * Creates a normal AI with 10 in all attributes.
+     * 
+     * @param color The custom color for this AI
+     */
     public BaseAI(Color color) {
         super(color,10,10,10,10);
     }
     
+    /**
+     * Returns its own classpath.
+     * 
+     * @return its own classpath
+     */
     @Override
     public String AIclass() {
         return "planets.entities.AIs.BaseAI";
