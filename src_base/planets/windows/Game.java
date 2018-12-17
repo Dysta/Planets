de303ab0aec0296482eec446bb0b4f7bfd16dfcc
Unknown <adrien.boitelle@gmail.com>
2018-12-18 00:41:15 +0100

    Window & Game javadoc

--- src_base/planets/windows/Game.java
+++ src_base/planets/windows/Game.java
@@ -6,18 +6,13 @@ import input.KeyEventHandler;
 import input.MouseHandler;
 import input.MouseReleaseHandler;
 import input.ScrollEventHandler;
-import javafx.event.EventHandler;
 import javafx.scene.CacheHint;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
 import planets.Planets;
 import planets.ResourcesManager;
 import planets.Sprite;
@@ -26,29 +21,68 @@ import planets.entities.planet.Planet;
 import planets.entities.Player;
 import planets.entities.Mission;
 import planets.entities.Squad;
-import planets.utils.DebugUtils;
 import ui.SelectPercentage;
 import planets.entities.ship.Ship;
 
+/**
+ * The actual Game window, with every entity in it. It handles the game and events related to it.
+ * 
+ * @author Adri
+ */
 public class Game extends Window {
 
-    // Entities
+    
+    /**
+     * The galaxy containing entities.
+     */
     public static Galaxy galaxy;
+    /**
+     * A static collection of the current missions.
+     */
     public static ArrayList<Mission> missions;
+    /**
+     * A static reference to the main player.
+     */
     public static Player mainPlayer;
 
     // States
+    /**
+     * Indicates whether the user has been holding mouse1.
+     */
     public static boolean primaryHeld;
+    /**
+     * Indicates whether the user has been holding control.
+     */
     public static boolean ctrlHeld;
+    /**
+     * If set to true, prevents the game from handling anything.
+     */
     public static boolean freeze = false;
+    /**
+     * The currently selected planets.
+     */
     public static ArrayList<Planet> selectedPlanets;
+    /**
+     * The currently selected Squads.
+     */
     public static ArrayList<Squad> selectedSquads;
+    /**
+     * An array of ticks to compute tickrate.
+     */
     public static int ticks;
 
     // UI Elements
+    /**
+     * The indicator showing the current percentage of selected ships.
+     */
     public static SelectPercentage selectP;
 
-    // Methods
+    /**
+     * Creates an empty game and defines input events.
+     * 
+     * @param WIDTH the width of the window
+     * @param HEIGHT the height of the window
+     */
     @Override
     public void init(double WIDTH, double HEIGHT) {
         this.WIDTH = WIDTH;
@@ -76,6 +110,10 @@ public class Game extends Window {
         stage.show();
     }
 
+    /**
+     * Prepares every needed event.
+     * @param scene the scene listening to event
+     */
     private void initEvents(Scene scene) {
         MouseHandler mouseHandler = new MouseHandler();
         MouseReleaseHandler mouseReleaseHandler = new MouseReleaseHandler();
@@ -89,6 +127,12 @@ public class Game extends Window {
         scene.setOnScroll(scrollEventHandler);
     }
 
+    /**
+     * Creates a random game from given parameters
+     * 
+     * @param nbPlayers the maximum number of players
+     * @param nbPlanets the maximum number of planets
+     */
     public void initGame(int nbPlayers, int nbPlanets) {
         double borderMargin = 50;
 
@@ -112,6 +156,12 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Update the current game to match given arguments
+     * 
+     * @param galaxy the new galaxy reference
+     * @param missions the new missions reference
+     */
     public void load(Galaxy galaxy, ArrayList<Mission> missions) {
         Game.root.getChildren().clear();
         Game.galaxy = galaxy;
@@ -132,14 +182,28 @@ public class Game extends Window {
             }
         }
     }
-    // Game instantiation 
-    // Game behavior
-
+    
+    /**
+     * Starts a new mission
+     * 
+     * @param origin The origin planet
+     * @param destination The destination planet
+     * @param effectives The amount of ships to send
+     * @param squadSize The max squad size
+     * @param mission The goal, @see Window.
+     */
     public static void startMission(Planet origin, Planet destination, int effectives, int squadSize, String mission) {
         Mission r = new Mission(origin, destination, effectives, squadSize, mission);
         Game.missions.add(r);
     }
 
+    /**
+     * Changes a squad's mission while they wer eperforming another.
+     * 
+     * @param squads the squad in question
+     * @param destination the new destination
+     * @param newOrder the new goal
+     */
     public static void changeMission(ArrayList<Squad> squads, Planet destination, String newOrder) {
         Mission r = new Mission(squads.get(0).getMission().getOriginPlanet(), destination, 0, 0, newOrder);
 
@@ -151,6 +215,13 @@ public class Game extends Window {
         Game.missions.add(r);
     }
 
+    /**
+     * Sets any element selected in a matching list.
+     * 
+     * @param list the select list corresponding to the element
+     * @param element the element
+     * @param state whether it's selected or not
+     */
     public static void setSelect(ArrayList list, Sprite element, boolean state) {
         if (state) {
             list.add(element);
@@ -160,6 +231,10 @@ public class Game extends Window {
         element.setSelected(state);
     }
 
+    /**
+     * Removes every element from a selection list.
+     * @param list 
+     */
     public static void deselect(ArrayList list) {
         int c = 0;
         int t = list.size();
@@ -169,14 +244,27 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Change the game freeze status.
+     * @param b new status
+     */
     public static void setFreeze(boolean b) {
         Game.freeze = b;
     }
 
+    /**
+     * Reverts the current game freeze status.
+     */
     public static void toggleFreeze() {
     	setFreeze(!Game.freeze);
     }
 
+    /**
+     * Toggles selection on a squad 
+     * 
+     * @param s the squad
+     * @param state the new state
+     */
     public static void setSelectSquad(Squad s, boolean state) {
         if (state) {
             Game.selectedSquads.add(s);
@@ -189,6 +277,9 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Resets all selected squads.
+     */
     public static void deselectSquads() {
         int c = 0;
         int t = Game.selectedSquads.size();
@@ -198,6 +289,11 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Creates a convoy mission.
+     * 
+     * @param target The target planet
+     */
     public static void startConvoy(Planet target) {
         for (Planet o : Game.selectedPlanets) {
             Game.startMission(o, target, (int) (o.getNbShip() * (o.getOwner().getEffectivesPercent() / 100)), o.getMaxLaunchShips(), Mission.CONVOY);
@@ -205,10 +301,22 @@ public class Game extends Window {
         Game.deselect(Game.selectedPlanets);
     }
 
+
+    /**
+     * Creates an attack mission.
+     * 
+     * @param o The origin planet
+     * @param target The target planet
+     */
     public static void startAttack(Planet o, Planet target) {
         Game.startMission(o, target, (int) (o.getNbShip() * (o.getOwner().getEffectivesPercent() / 100)), o.getMaxLaunchShips(), Mission.ATTACK);
     }
 
+    /**
+     * Terminate all missions for a planet.
+     * 
+     * @param p the planet to clear
+     */
     public static void endPlanetMissions(Planet p) {
         if(Game.missions != null) {
             for (Mission m : Game.missions) {
@@ -219,6 +327,11 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Handles every game tick's behavior.
+     * 
+     * @param arg0 unused but provided by Application
+     */
     public void handle(long arg0) {
         if (!Game.freeze) {
             if(isGameOver()) {
@@ -240,6 +353,9 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Handles the refresh of the game's user interface
+     */
     public void updateUI() {
         if (!Game.freeze) {
             for (Planet p : Galaxy.getPlanets()) {
@@ -250,6 +366,9 @@ public class Game extends Window {
         }
     }
 
+    /**
+     * Makes the AIs take their decisions
+     */
     public void handleAI() {
         if (!Game.freeze) {
             for (Player p : Galaxy.getPlayers()) {
@@ -258,12 +377,19 @@ public class Game extends Window {
         }
     }
     
+    /**
+     * Clear every element from the Game window.
+     */
     @Override
     public void clear() {
         Game.freeze = true;
         root.getChildren().clear();
     }
 
+    /**
+     * Checks whether a game is over or not.
+     * @return true if the game is over.
+     */
     public boolean isGameOver() {
         boolean hasEnemy = false;
         boolean hasMainPlayer = false;
